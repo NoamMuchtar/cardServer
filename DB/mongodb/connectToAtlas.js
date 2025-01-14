0f4035028031d1a4a885533ac47779103e4076c6
NoamMuchtar <noam@nomu.co.il>
2025-01-14 14:23:39 +0200

    final commit

--- DB/mongodb/connectToAtlas.js
+++ DB/mongodb/connectToAtlas.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-const connectionStringForAtlas = "";
+require("dotenv").config();
+const connectionStringForAtlas = process.env.ATLAS_CONNECTION_STRING;
 
 const connectToAtlasDb = async () => {
   try {
-    // await mongoose.connect(connectionStringForAtlas);
+    await mongoose.connect(connectionStringForAtlas);
     console.log("Connected to MongoDB in Atlas");
   } catch (error) {
     console.error("Could not connect to MongoDB", error);
